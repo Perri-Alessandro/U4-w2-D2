6970ea3c544198b7947684043f9d3f97c3ad6315
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-03-12 17:01:08 +0100

    exercise 3 to implement

--- src/entities/es_3/Terzo.java
+++ src/entities/es_3/Terzo.java
@@ -12,23 +12,46 @@ public class Terzo {
 
         System.out.println("RUBRICA");
 
-        String nome;
+        int scelta;
         do {
-            System.out.println("INSERISCI UNA COPPIA NOME-TELEFONO. INIZIA INSERENDO IL NOME (lascia vuoto per terminare):");
-            nome = sc.nextLine();
+            System.out.println("SCEGLI UN OPZIONE:");
+            System.out.println("1. AGGIUNGI COPPIA NOME-TELEFONO");
+            System.out.println("2. RIMUOVI COPPIA NOME-TELEFONO");
+            System.out.println("3. ESCI");
+            scelta = Integer.parseInt(sc.nextLine());
 
-            if (!nome.isEmpty()) {
-                System.out.println("ORA INSERISCI IL NUMERO DI TELEFONO :");
-                long numero = Long.parseLong(sc.nextLine());
-
-                rubricaMap.put(nome, numero);
-
-                System.out.println("AGGIUNTO: " + nome + " - " + numero);
+            switch (scelta) {
+                case 1:
+                    System.out.println("INSERISCI IL NOME (lascia vuoto per terminare): ");
+                    String nome = sc.nextLine();
+                    if (!nome.isEmpty()) {
+                        System.out.println("ORA INSERISCI IL NUMERO DI TELEFONO:");
+                        long numero = Long.parseLong(sc.nextLine());
+                        rubricaMap.put(nome, numero);
+                        System.out.println("AGGIUNTO: " + nome + " - " + numero);
+                    }
+                    System.out.println("RUBRICA AGGIORNATA: " + rubricaMap);
+                    break;
+                case 2:
+                    System.out.println("INSERISCI IL NOME DA RIMUOVERE: ");
+                    String nomeDaRimuovere = sc.nextLine();
+                    if (rubricaMap.containsKey(nomeDaRimuovere)) {
+                        rubricaMap.remove(nomeDaRimuovere);
+                        System.out.println("RIMOSSO: " + nomeDaRimuovere);
+                    } else {
+                        System.out.println("NOME NON TROVATO IN RUBRICA.");
+                    }
+                    System.out.println("RUBRICA AGGIORNATA: " + rubricaMap);
+                    break;
+                case 3:
+                    System.out.println("USCITA IN CORSO ...");
+                    break;
+                default:
+                    System.out.println("OPZIONE NON VALIDA, RIPROVA");
+                    break;
             }
 
-        } while (!nome.isEmpty());
-
-        System.out.println("RUBRICA AGGIORNATA: " + rubricaMap);
+        } while (scelta != 3);
 
 
         sc.close();
